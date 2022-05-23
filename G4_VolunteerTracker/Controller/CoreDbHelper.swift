83d7d58e53a5960f06bba9f153f8b42dbfc63b86
Sadaf Asad <sadaf98x@gmail.com>
2022-05-23 14:47:40 -0400

    CRUD operations for events and volunteers added--not tested

--- G4_VolunteerTracker/Controller/CoreDbHelper.swift
+++ G4_VolunteerTracker/Controller/CoreDbHelper.swift
@@ -15,6 +15,8 @@ class CoreDbHelper{
     
     private let moc : NSManagedObjectContext
     private let USER_ENTITY_NAME = "User"
+    private let EVENT_ENTITY_NAME = "Event"
+    private let VOLUNTEER_ENTITY_NAME = "Volunteer"
 
     //MARK: init
     private init(context : NSManagedObjectContext){
@@ -31,6 +33,7 @@ class CoreDbHelper{
         return shared!
     }
   
+    // Create Operations
     func createUser(name: String, email: String, password: String){
         //helper method for placing a new user
         do{
@@ -52,6 +55,49 @@ class CoreDbHelper{
             print(#function, "Could not save the user \(error)")
         }
     }
+    
+    func createEvent(name: String, info: String, organization: String, location: String, date: Date, start_time: Date, end_time: Date) {
+        do {
+            let eventToCreate = NSEntityDescription.insertNewObject(forEntityName: EVENT_ENTITY_NAME, into: self.moc) as! Event
+        
+            eventToCreate.name = name
+            eventToCreate.info = info
+            eventToCreate.organization = organization
+            eventToCreate.location = location
+            eventToCreate.date = date
+            eventToCreate.start_time = start_time
+            eventToCreate.end_time = end_time
+            eventToCreate.id = UUID()
+        
+            if (self.moc.hasChanges) {
+                try self.moc.save()
+                print(#function, "Event is saved successfully into CoreData")
+            }
+        } catch let error as NSError {
+            print(#function, "Could not save the event \(error)")
+        }
+    }
+    
+    func createVolunteer() {
+        do {
+            let volunteerToCreate = NSEntityDescription.insertNewObject(forEntityName: VOLUNTEER_ENTITY_NAME, into: self.moc) as! Volunteer
+            
+            volunteerToCreate.attend = false
+            volunteerToCreate.hours = 0.0
+            volunteerToCreate.location = nil
+            volunteerToCreate.picture = nil
+            volunteerToCreate.id = UUID()
+            
+            if (self.moc.hasChanges) {
+                try self.moc.save()
+                print(#function, "Volunteer is saved successfully into CoreData")
+            }
+        } catch let error as NSError {
+            print(#function, "Could not save the volunteer \(error)")
+        }
+    }
+    
+    // Read Operations
     func getAllUsers() -> [User]?{
         let fetchRequest = NSFetchRequest<User>(entityName: USER_ENTITY_NAME)
         
@@ -66,6 +112,20 @@ class CoreDbHelper{
         }
         return nil
     }
+    
+    func getAllEvents() -> [Event]? {
+        let fetchRequest = NSFetchRequest<Event>(entityName: EVENT_ENTITY_NAME)
+        do {
+            let result = try self.moc.fetch(fetchRequest)
+            print (#function, "Fetched Data : \(result as [Event])")
+            return result as [Event]
+        } catch let error as NSError {
+            print(#function, "Could not fetch the data \(error)")
+        }
+        return nil
+    }
+    
+    // Read Operations (specific)
     func searchUser(email : String) -> User?{
         
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: USER_ENTITY_NAME)
@@ -86,6 +146,62 @@ class CoreDbHelper{
         }
         
         return nil
+    }
+    
+    func searchEvent(eventID: UUID) -> Event? {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: EVENT_ENTITY_NAME)
+        let predicateID = NSPredicate(format: "id == %@", eventID as CVarArg)
+        fetchRequest.predicate = predicateID
+        
+        do {
+            let result = try self.moc.fetch(fetchRequest)
+            if (result.count > 0) {
+                print(#function, "Matching object found")
+                return result.first as? Event
+            }
+        } catch let error as NSError {
+            print(#function, "Unable to search for event \(error)")
+        }
+        return nil
+    }
+    
+    func searchVolunteer(volunteerID: UUID) -> Volunteer? {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: VOLUNTEER_ENTITY_NAME)
+        let predicateID = NSPredicate(format: "id == %@", volunteerID as CVarArg)
+        fetchRequest.predicate = predicateID
+        
+        do {
+            let result = try self.moc.fetch(fetchRequest)
+            if (result.count > 0) {
+                print(#function, "Matching object found")
+                return result.first as? Volunteer
+            }
+        } catch let error as NSError {
+            print(#function, "Unable to search for volunteer \(error)")
+        }
+        return nil
+    }
+    
+    // Update Operations
+    func updateVolunteer(updatedVolunteer: Volunteer) {
+        let searchResult = self.searchVolunteer(volunteerID: updatedVolunteer.id! as UUID)
+        if (searchResult != nil) {
+            do {
+                let volunteerToUpdate = searchResult!
+                
+                volunteerToUpdate.attend = true
+                volunteerToUpdate.hours = updatedVolunteer.hours
+                volunteerToUpdate.location = updatedVolunteer.location
+                volunteerToUpdate.picture = updatedVolunteer.picture
+                
+                try self.moc.save()
+                print(#function, "Volunteer updated successfully")
+            } catch let error as NSError {
+                print(#function, "Could not update the volunteer \(error)")
+            }
+        } else {
+            print(#function, "No matching record found")
+        }
     }
 }
 
